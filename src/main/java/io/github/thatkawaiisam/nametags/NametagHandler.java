78766f49b6af8d89f2c0723e84cd41b40b95a748
ThatKawaiiSam <sam.sergi@outlook.com>
2019-06-09 19:31:50 +1000

    Added friendly invis and show health.

--- src/main/java/io/github/thatkawaiisam/nametags/NametagHandler.java
+++ src/main/java/io/github/thatkawaiisam/nametags/NametagHandler.java
@@ -2,9 +2,13 @@ package io.github.thatkawaiisam.nametags;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
@@ -15,10 +19,11 @@ public class NametagHandler {
 
     private NametagAdapter adapter;
     private JavaPlugin plugin;
+    private NametagThread thread;
 
     public NametagHandler(JavaPlugin plugin) {
         this.plugin = plugin;
-        NametagThread thread = new NametagThread(this, 100);
+        thread = new NametagThread(this, 10);
         thread.start();
     }
 
@@ -27,33 +32,58 @@ public class NametagHandler {
         this.adapter = adapter;
     }
 
+    public void cleanup() {
+        thread.stop();
+    }
+
     public void update(Player player) {
         if (adapter == null) {
             return;
         }
         List<BufferedNametag> nametags = adapter.getPlate(player);
         Scoreboard sb = player.getScoreboard();
+
+        if (sb == Bukkit.getScoreboardManager().getMainScoreboard()) {
+            return;
+        }
+
         for (BufferedNametag bufferedNametag : nametags) {
+            //Get Team
             Team team = sb.getTeam(bufferedNametag.getGroupName());
+
             if (team == null) {
                 team = sb.registerNewTeam(bufferedNametag.getGroupName());
             }
-            team = sb.getTeam(bufferedNametag.getGroupName());
+
+            //Set Prefix
             if (bufferedNametag.getPrefix() != null) {
                 team.setPrefix(bufferedNametag.getPrefix());
             } else {
                 team.setPrefix(ChatColor.WHITE.toString());
             }
+            //Set Suffix
             if (bufferedNametag.getSuffix() != null) {
                 team.setSuffix(bufferedNametag.getSuffix());
             } else {
                 team.setSuffix(ChatColor.WHITE.toString());
             }
-            if (team != null && bufferedNametag.getPlayer() != null && bufferedNametag.getPlayer().isOnline()) {
-                team.addPlayer(bufferedNametag.getPlayer());
+            if (bufferedNametag.getPlayer() != null && bufferedNametag.getPlayer().isOnline()) {
+                team.addEntry(bufferedNametag.getPlayer().getName());
             }
-            if (team != null && player != null && player.isOnline()) {
-                team.addPlayer(player);
+
+            //Friendly Invis
+            team.setCanSeeFriendlyInvisibles(bufferedNametag.isFriendlyInvis());
+
+            if (bufferedNametag.isShowHealth() && sb.getObjective(DisplaySlot.BELOW_NAME) == null) {
+                Objective objective = sb.registerNewObjective("showhealth", "health");
+                objective.setDisplaySlot(DisplaySlot.BELOW_NAME);
+                objective.setDisplayName(ChatColor.RED + StringEscapeUtils.unescapeJava("\u2764"));
+                objective.getScore(bufferedNametag.getPlayer()).setScore((int) Math.floor(bufferedNametag.getPlayer().getHealth()));
+            }
+
+            if (!bufferedNametag.isShowHealth() && sb.getObjective(DisplaySlot.BELOW_NAME) != null) {
+                Objective objective = sb.getObjective(DisplaySlot.BELOW_NAME);
+                objective.unregister();
             }
         }
     }
